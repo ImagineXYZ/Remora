db4cdc141a37289d6b128ac88ef640d5b35d4bd3
Sergio Vargas Delgado <savd_08@outlook.com>
2017-04-10 19:25:50 -0600

    Agrega archivos de soporte para wireless debug

---
+++ WDebug/wdebug_ex/wdebug_ex.ino
@@ -0,0 +1,56 @@
+//Bibliotecas que se deben incluir
+#include <ESP8266_XYZ.h>
+#include "wiring_private.h" //pinPeripheral
+
+#define SSID F("WDebug")  //Nombre de la red a la que se desea conectar
+#define PASS F("espdebug")   //Contraseña de la red
+#define Serial SERIAL_PORT_USBVIRTUAL
+
+Uart WSerial (&sercom1, 13, 11, SERCOM_RX_PAD_1, UART_TX_PAD_0);
+
+void SERCOM1_Handler()
+{
+  WSerial.IrqHandler();
+}
+
+ESP8266_XYZ esp(&WSerial);    //Arg #1: puntero al stream, Arg #2: pin para reset
+
+int n = 20000;
+int m = 20000;
+
+void setup() {
+  
+  
+  Serial.begin(9600);             //Inicializacion del Monitor Serial a 9600
+  while(!Serial);    
+  Serial.println("Init");         //Mensaje de inicialización
+  WSerial.begin(9600);           //Inicializacion de SoftwareSerial a 9600
+  Serial.println("Init WSerial");         //Mensaje de inicialización
+
+  pinPeripheral(13, PIO_SERCOM);
+  pinPeripheral(11, PIO_SERCOM);
+  
+  //No se continúa hasta asegurar el buen funcionamiento del dispositivo
+  while (!esp.espTest());
+  Serial.println("ESP Test");  
+  while (!esp.softReset());
+  Serial.println("ESP Reset");
+  esp.connectAP(SSID, PASS);
+  Serial.println("ESP AP");
+  
+  Serial.println("Setup OK");     //Si se muestra existe conexión
+  
+}
+
+void loop() {
+
+  esp.println(String(n));
+  Serial.println(String(n));
+
+  //Se incrementa contador de ejemplo 
+  n++;
+  //Se decrementa el contador de ejemplo
+  m--;
+  
+  delay(1000);         //Suspende operaciones por 1 segundos
+}
