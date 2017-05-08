e64cf5750badaa0e0ea75302166792bbca67bbc4
Sergio Vargas Delgado <savd_08@outlook.com>
2017-05-08 17:47:27 -0600

    Agrega escalamiento al temporizador del WDT

--- samd21_wdt/samd_wdt_test/samd_wdt_test.ino
+++ samd21_wdt/samd_wdt_test/samd_wdt_test.ino
@@ -4,6 +4,10 @@ static void WDTsync() {
   while (WDT->STATUS.bit.SYNCBUSY == 1); //Sincronización del WDT
 }
 
+static void GCLKsync() {
+  while (GCLK->STATUS.bit.SYNCBUSY == 1); //Sincronización del GCLK
+}
+
 void setup() {
   Serial.begin(115200);
   delay(15000); //Tiempo para inicializar hardware
@@ -49,13 +53,20 @@ void disableWDT(){
 }
 
 void setupWDT(uint8_t period) {
-
   WDT->CTRL.reg = 0; // Se desactiva WDT
   WDTsync(); // Sincronizar para no perder configuración
-
+  
+  GCLK->GENDIV.reg = 0x1F02; //Establece la división del generador de relojes (GCLKGEN2/2^5)
+  GCLKsync(); // Sincronizar para no perder configuración
+  
+  GCLK->GENCTRL.reg = 0x110302; //Relaciona OSCULP32K con GCLKGEN2
+  GCLKsync(); // Sincronizar para no perder configuración
+  
+  GCLK->CLKCTRL.reg = 0x4203; //Relaciona GCLKGEN2 con GCLK_WDT
+  GCLKsync(); // Sincronizar para no perder configuración
+  
   WDT->CONFIG.reg = min(period, 11); // Tabla 17-5 Timeout Period 
 
   WDT->CTRL.reg = WDT_CTRL_ENABLE; //Activar WDT
   WDTsync(); // Sincronizar para no perder configuración
 }
-
