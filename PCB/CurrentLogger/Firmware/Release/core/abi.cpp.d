0c5ca61f16b7b2e7306b6e5e1d52c54dba622efa
Bryan Salazar <bryanmtdt@gmail.com>
2017-05-31 13:34:56 -0600

    Medidor de Energía, PCB, Current Logger

---
+++ PCB/CurrentLogger/Firmware/Release/core/abi.cpp.d
@@ -0,0 +1,2 @@
+core/abi.cpp.o: \
+ /home/bryanmtdt/Documents/sloeber/arduinoPlugin/packages/adafruit/hardware/samd/1.0.17/cores/arduino/abi.cpp
---
+++ PCB/CurrentLogger/Firmware/Release/core/hooks.c.d
@@ -0,0 +1,2 @@
+core/hooks.c.o: \
+ /home/bryanmtdt/Documents/sloeber/arduinoPlugin/packages/adafruit/hardware/samd/1.0.17/cores/arduino/hooks.c
